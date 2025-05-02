ebb96239f3f8baddb7f77b81620182aaedf3666f
Arch_lucas <lucas27ro@gmail.com>
2025-05-02 00:23:23 -0300

    Add @phosphor-icons/react package with license, README, and package.json
    - Introduced the @phosphor-icons/react package, including the MIT license.
    - Added a comprehensive README.md detailing installation, usage, props, context, and custom icons.
    - Configured package.json with necessary metadata, dependencies, and scripts for building and testing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import { Header } from "./components/Header"
-import { LeftBar } from "./components/LeftBar"
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { LeftBar } from "./components/LeftBar";
 
-import './style/global.css'
+import './style/global.css';
 
 export function App() {
+  const [isLeftBarVisible, setIsLeftBarVisible] = useState(true);
+
+  const toggleLeftBar = () => {
+    setIsLeftBarVisible(!isLeftBarVisible);
+  };
+
   return (
     <div>
-      <Header/>
-      <LeftBar/>
-
+      <Header toggleLeftBar={toggleLeftBar} />
+      {isLeftBarVisible && <LeftBar />}
     </div>
-  )
+  );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,20 @@
-import styles from '../style/Header.module.css'
+import styles from '../style/Header.module.css';
 
-export function Header() {
+import { List } from "@phosphor-icons/react"
 
+export function Header({ toggleLeftBar }) {
     return (
         <>
         <header className={styles.header}>
-            <button className={styles.button}>=</button>
+            {/* <button className={styles.button} onClick={toggleLeftBar}><img className={styles.buttonImg} src={icoHeader} alt="" /></button> */}
+            <button className={styles.button} onClick={toggleLeftBar}><List size={48} color='blue'/></button>
             <h2 className={styles.title_main}>BUG HUNTER</h2>
             <div className={styles.div_user}>
                 <h4>20?</h4>
                 <h4>Username</h4>
-                <img src="#" alt="" />
+                <img src="https://img.freepik.com/vetores-premium/ilustracao-em-vetor-3d-realista-de-pessoa-ou-pessoas_156780-142.jpg" alt="" />
             </div>
         </header>
         </>
-    )
+    );
 }
